f281194315d01bb5a0125f89965f3ce89f7ccc21
sunm10 <sunm2@nycstudents.net>
2025-11-19 17:34:54 -0500

    Update dependencies

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -5,6 +5,7 @@ import com.bylazar.panels.Panels;
 import com.bylazar.telemetry.PanelsTelemetry;
 import com.bylazar.telemetry.TelemetryManager;
 import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.Mechanisms.MecanumDrive;
@@ -14,7 +15,6 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 // adb connect 192.168.43.1:5555 (Connect to the Control Hub via ADB)
 // http://192.168.43.1:8001 FTC Panel
-
 @Configurable
 @TeleOp(name = "TeleOp")
 public class Robot extends OpMode {
@@ -30,27 +30,20 @@ public class Robot extends OpMode {
 
         drive.init(hardwareMap);
         telemetryM = PanelsTelemetry.INSTANCE.getTelemetry();
-        telemetryM.debug("Heading", drive.getHeading(AngleUnit.DEGREES));
-        telemetryM.update(telemetry);
+        telemetryM.debug("Heading: ", drive.getHeading(AngleUnit.DEGREES));
 
+
+        telemetryM.update();
     }
 
     @Override
     public void loop() {
 
         forward = gamepad1.left_stick_y;
-        strafe = gamepad1.left_stick_x;
-        rot = gamepad1.right_stick_x;
+        strafe = -gamepad1.left_stick_x;
+        rot = -gamepad1.right_stick_x;
 
-        if(gamepad1.a) {
-            drive.fieldRelativeDrive(forward,strafe,rot);
-            mode = "Field Relative";
-        }
-
-        if(gamepad1.b) {
-            drive.drive(forward,strafe,rot);
-            mode = "Robot Oriented";
-        }
+        drive.fieldRelativeDrive(forward,strafe,rot);
     }
 }
 
