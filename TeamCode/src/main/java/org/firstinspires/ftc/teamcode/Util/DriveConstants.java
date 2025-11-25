b5fe6162ddb82386c44f99f672d100fea947ff25
Navjot42 <navtoxic@gmail.com>
2025-11-25 16:51:14 -0500

    Create DriveConstants.java
    created drive constant class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/DriveConstants.java
@@ -1,4 +1,16 @@
 package org.firstinspires.ftc.teamcode.Util;
 
 public class DriveConstants {
+    // wheel + encoder
+    //public static double TICKS_PER_REV = 537.7;
+    //public static double WHEEL_RADIUS = 1.889;  // inches
+    //public static double GEAR_RATIO = 1.0;
+
+    // robot size
+    public static double ROBOT_WIDTH = 17.785;  // inches
+    public static double ROBOT_LENGTH = 17.323; // inches
+
+    // speed limits
+    public static double MAX_POWER = 1.0;
+    public static double MAX_SPEED = 1.0;
 }
