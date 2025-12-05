c89a43a9db28b0c4d7da94ecff701553e1ee602d
sunm10 <sunm2@nycstudents.net>
2025-12-05 17:58:23 -0500

    Create Auto.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/Auto.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.OpModes.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "Auto", group = "Robot")
+public class Auto {
+
+}
