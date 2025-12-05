8c155f65785d907a923ef3724fb00b301b7e2b03
Navjot42 <navtoxic@gmail.com>
2025-12-05 16:27:15 -0500

    Update Constants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -14,7 +14,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 public class Constants {
 
     public static FollowerConstants followerConstants = new FollowerConstants()
-            .mass(5.9); // MUST BE IN KG
+            .mass(4.8); // MUST BE IN KG
 
     public static MecanumConstants driveConstants = new MecanumConstants()
             .maxPower(1)
@@ -46,7 +46,7 @@ public class Constants {
             .leftRearEncoderDirection(Encoder.FORWARD)
             .rightFrontEncoderDirection(Encoder.REVERSE)
             .rightRearEncoderDirection(Encoder.REVERSE)
-            .robotWidth(17.785)
-            .robotLength(17.323)
+            .robotWidth(17)
+            .robotLength(18)
             .forwardTicksToInches(-0.0041770665874543735);
 }
