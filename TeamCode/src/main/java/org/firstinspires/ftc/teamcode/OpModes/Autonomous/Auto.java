81d90c0ef0874aac40d8d184c16d8cc38f13f92b
sunm10 <sunm2@nycstudents.net>
2025-12-17 16:03:27 -0500

    Auto class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/Auto.java
@@ -1,8 +1,16 @@
 package org.firstinspires.ftc.teamcode.OpModes.Autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+@Disabled
 @Autonomous(name = "Auto", group = "Robot")
-public class Auto {
+public class Auto extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
 
 }
