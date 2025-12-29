3ec1fb8dcd811ae46c3d56ce106368034ca345b4
sunm10 <sunm2@nycstudents.net>
2025-12-29 15:08:01 -0500

    changed motor object name

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Tilt.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Tilt.java
@@ -5,30 +5,30 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Tilt {
 
-    private DcMotor motor;
-    final int motorTicksPerRev = 28; // Ticks per motor revolution
-    final int gearRatio = 125; // Total gear reduction (5^3)
-    final int outputRev = 12941; // PLACEHOLDER
-    final int targetPos = motorTicksPerRev * gearRatio * outputRev;
+    private DcMotor tiltMotor;
+    private final int motorTicksPerRev = 28; // Ticks per motor revolution
+    private final int gearRatio = 125; // Total gear reduction (5^3)
+    private final int outputRev = 12941; // PLACEHOLDER
+    private final int targetPos = motorTicksPerRev * gearRatio * outputRev;
 
     private boolean isTilted = false;
 
     public void init(HardwareMap hwMap) {
-        motor = hwMap.get(DcMotor.class, "TiltMotor");
-        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        motor.setTargetPosition(targetPos);
+        tiltMotor = hwMap.get(DcMotor.class, "TiltMotor");
+        tiltMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        tiltMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        tiltMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        tiltMotor.setTargetPosition(targetPos);
     }
 
     public void tilt(boolean tiltBind) {
         if (tiltBind && !isTilted) {
-            motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            tiltMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             isTilted = true;
         }
         if (tiltBind && isTilted) {
-            motor.setPower(0.5);
-            motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            tiltMotor.setPower(0.5);
+            tiltMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             isTilted = false;
         }
     }
