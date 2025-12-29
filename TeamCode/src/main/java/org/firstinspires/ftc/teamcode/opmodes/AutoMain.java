83a659c2558754dd8e62e42c868ca5c742997c22
abdumalik-sh <41bestww@gmail.com>
2025-12-29 12:53:47 -0500

    might work, needs to test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutoMain.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.robot.Robot;
+
+public class TeleOpMain extends OpMode {
+
+    private Robot robot;
+
+    @Override
+    public void init() {
+        robot = new Robot(hardwareMap);
+    }
+
+
+    @Override
+    public void loop() {
+
+        // Drive
+        double forward = gamepad1.left_stick_y;
+        double strafe = -gamepad1.left_stick_x;
+        double rot = -gamepad1.right_stick_x;
+
+        robot.drive.fieldRelativeDrive(forward,strafe,rot);
+        
+    }
+
+}
