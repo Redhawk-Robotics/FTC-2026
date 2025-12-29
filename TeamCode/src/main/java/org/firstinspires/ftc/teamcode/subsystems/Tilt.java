61b993f3abeb7ee566ea8e0979e2763261d9a433
sunm10 <sunm2@nycstudents.net>
2025-12-29 15:00:21 -0500

    target pos = motor revolutions from pos 0

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Tilt.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Tilt.java
@@ -6,12 +6,16 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 public class Tilt {
 
     private DcMotor motor;
-    private int targetPos = 1; // PLACEHOLDER VALUE
+    final int motorTicksPerRev = 28; // Ticks per motor revolution
+    final int gearRatio = 125; // Total gear reduction (5^3)
+    final int outputRev = 12941; // PLACEHOLDER
+    final int targetPos = motorTicksPerRev * gearRatio * outputRev;
+
     private boolean isTilted = false;
 
     public void init(HardwareMap hwMap) {
         motor = hwMap.get(DcMotor.class, "TiltMotor");
-        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); // .2 TICK PER REV
+        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         motor.setTargetPosition(targetPos);
