59832e99002e697fe0831ae104371d4557b0c162
Navjot42 <navtoxic@gmail.com>
2025-12-31 11:06:20 -0500

    updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOpMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOpMain.java
@@ -23,12 +23,13 @@ public class TeleOpMain extends OpMode {
         double forward = gamepad1.left_stick_y;
         double strafe = -gamepad1.left_stick_x;
         double rot = -gamepad1.right_stick_x;
-
         robot.drive.fieldRelativeDrive(forward,strafe,rot);
 
         // Tilt
         boolean tiltBind = gamepad1.b;
-        robot.tilt.tilt(tiltBind);
+        boolean reduceTilt = gamepad1.left_bumper;
+        boolean increaseTilt = gamepad1.right_bumper;
+        robot.tilt.tilt(reduceTilt,increaseTilt,tiltBind);
 
     }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Tilt.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Tilt.java
@@ -1,36 +1,39 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.bylazar.configurables.annotations.Configurable;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+@Configurable
 public class Tilt {
 
     private DcMotor tiltMotor;
     private final int motorTicksPerRev = 28; // Ticks per motor revolution
     private final int gearRatio = 125; // Total gear reduction (5^3)
-    private final int outputRev = 12941; // PLACEHOLDER
-    private final int targetPos = motorTicksPerRev * gearRatio * outputRev;
+    private final double outputRev = 0.4;
+    private final double targetPos = motorTicksPerRev * gearRatio * outputRev;
 
-    private boolean isTilted = false;
 
     public void init(HardwareMap hwMap) {
         tiltMotor = hwMap.get(DcMotor.class, "TiltMotor");
         tiltMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         tiltMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         tiltMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        tiltMotor.setTargetPosition(targetPos);
+        tiltMotor.setTargetPosition((int) targetPos);
     }
 
-    public void tilt(boolean tiltBind) {
-        if (tiltBind && !isTilted) {
+    public void tilt(boolean reduceTilt, boolean increaseTilt, boolean tiltBind) {
+
+        if (tiltBind) {
             tiltMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            isTilted = true;
         }
-        if (tiltBind && isTilted) {
+
+        if (increaseTilt) {
             tiltMotor.setPower(0.5);
-            tiltMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            isTilted = false;
+        }
+
+        if (reduceTilt) {
+            tiltMotor.setPower(-0.5);
         }
     }
-
 }
